2563709f99b2468359886ffa8ee2c9cda2e061cb
Adrian Ross <adriantoddross@gmail.com>
2018-01-15 11:04:59 -0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//Function expression
+const sayHello = function(firstName, lastName) {
+    console.log('Hello', firstName, lastName);
+};
+
+//Calling sayHello Function
+sayHello('Joe', 'Turner');
+sayHello('Bhaumik', 'Patel');
